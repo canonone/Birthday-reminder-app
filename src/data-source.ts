4c134dc07f84c56de7b089bd19ec52ffe87faf73
christian umoh <umohchristian001@gmail.com>
2025-04-20 17:44:00 +0100

    feat: created a standalone file for the data source rather than importing in the app module

---
+++ src/data-source.ts
@@ -0,0 +1,20 @@
+import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+
+export const AppDataSource = new DataSource({
+    type:'postgres',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [__dirname + '/**/*.entity{.ts, .js}'],
+    synchronize: false,
+    migrations: [__dirname + '/migrations/*{.ts, .js}'],
+    migrationsRun: false,
+    logging: true,
+    migrationsTableName: 'migrations',
+    metadataTableName: "typeorm_metadata"
+})
