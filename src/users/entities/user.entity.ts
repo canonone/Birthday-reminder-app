2114832e40949e87391f1b865a013b9f1593716d
christian umoh <umohchristian001@gmail.com>
2025-04-20 17:42:51 +0100

    feat: created the user module and it's corresponding files

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn, Entity } from "typeorm";
+
+@Entity('user')
+export class User{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type:'varchar'})
+    username: string
+
+    @Column({ type:'varchar'})
+    email:string
+
+    @Column({ type: 'date'})
+    dateOfBirth:Date
+
+    @CreateDateColumn()
+    createdAt:Date
+
+    @UpdateDateColumn()
+    updatedAt:Date
+}
