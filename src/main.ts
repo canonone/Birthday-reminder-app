6f64ddb2630db2fa3f31b02d2156e78d03e92574
christian umoh <umohchristian001@gmail.com>
2025-04-21 12:19:34 +0100

    fixed the code format

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,19 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
-import { ValidationPipe } from '@nestjs/common'
-import { NestExpressApplication } from '@nestjs/platform-express'
-import {join} from "path"
-
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  
-  app.useGlobalPipes(
-    new ValidationPipe()
-  )
-  app.useStaticAssets(join('__dirname', '..', 'public'))
-  app.enableCors()
-  
-  const PORT = process.env.PORT || 3000
-  await app.listen(PORT, ()=>{
-    console.log(`the server is runnig on port:${PORT}`)
-  })
-  
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.useStaticAssets(join('__dirname', '..', 'public'));
+  app.enableCors();
+
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`the server is runnig on port:${PORT}`);
+  });
 }
-bootstrap()
+bootstrap();
