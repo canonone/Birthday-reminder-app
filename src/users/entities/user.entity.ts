0392a73723b5a7fe2303a367416f48f5c7917f30
christian umoh <umohchristian001@gmail.com>
2025-04-21 20:03:03 +0100

    fix: update the 'User' typo to 'user' to match the postgres db

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,22 +1,28 @@
-import { Column, PrimaryGeneratedColumn, UpdateDateColumn, CreateDateColumn, Entity } from "typeorm";
+import {
+  Column,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  CreateDateColumn,
+  Entity,
+} from 'typeorm';
 
-@Entity('User')
-export class User{
-    @PrimaryGeneratedColumn()
-    id: number;
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type:'varchar'})
-    username: string
+  @Column({ type: 'varchar' })
+  username: string;
 
-    @Column({ type:'varchar'})
-    email:string
+  @Column({ type: 'varchar' })
+  email: string;
 
-    @Column({ type: 'date'})
-    dateOfBirth:Date
+  @Column({ type: 'date' })
+  dateOfBirth: Date;
 
-    @CreateDateColumn()
-    createdAt:Date
+  @CreateDateColumn()
+  createdAt: Date;
 
-    @UpdateDateColumn()
-    updatedAt:Date
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
