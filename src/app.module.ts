db03b48b5a861e6ef608303648d809a97ff0c4de
christian umoh <umohchristian001@gmail.com>
2025-04-21 20:00:42 +0100

    include the data-source import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.modules';
 import { ScheduleModule } from '@nestjs/schedule';
+import dataSource from './data-source';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), UsersModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot(dataSource.options),
+  ],
 })
 export class AppModule {}
